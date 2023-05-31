4a6d4b390b4220ef80abca46940211ea39c36a60
zvihell <zvihell8@gmail.com>
2023-05-31 04:10:17 +0300

    add test cases

--- solutions/002-sum_of_elements/sum_test.go
+++ solutions/002-sum_of_elements/sum_test.go
@@ -3,12 +3,46 @@ package sumofelements
 import "testing"
 
 func TestSumofElements(t *testing.T) {
-	array := []int{2, 2, 4}
-	res := 8
+	testCases := []struct {
+		name     string
+		input    []int
+		expected int
+	}{
+		{
+			name:     "Пустой массив",
+			input:    []int{},
+			expected: 0,
+		},
+		{
+			name:     "Простой",
+			input:    []int{2, 3},
+			expected: 5,
+		},
+		{
+			name:     "Много чисел",
+			input:    []int{3, 3, 40, 4, 5, 20},
+			expected: 75,
+		},
+		{
+			name:     "1 число",
+			input:    []int{3},
+			expected: 3,
+		},
+		{
+			name:     "Отрицательные числа",
+			input:    []int{1, -2, -30},
+			expected: -31,
+		},
+	}
 
-	realRes := sum_of_elements(array)
-	if realRes != res {
-		t.Error(realRes)
+	for _, tc := range testCases {
+		println("Проверка теста " + tc.name + "..")
+		result := sum_of_elements(tc.input)
+		if result == tc.expected {
+			println("✅ Тест «" + tc.name + "» пройден")
+		} else {
+			t.Errorf("🤬 Тест «%v» провален, получено %v, ожидалось %v\n", tc.name, result, tc.expected)
+		}
 	}
 
 }
