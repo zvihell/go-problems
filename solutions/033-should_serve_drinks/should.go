7783dd1c212e3799fa797bf3b8dbdab8022a08dd
zvihell <zvihell8@gmail.com>
2023-06-23 08:23:19 +0300

    changing solution

--- solutions/033-should_serve_drinks/should.go
+++ solutions/033-should_serve_drinks/should.go
@@ -1,7 +1,10 @@
 package shouldservedrinks
 
 func should_serve_drinks(age int, on_break bool) bool {
-	if age < 18 || on_break == true {
+	if age < 18 {
+		return false
+	}
+	if on_break {
 		return false
 	}
 	return true
